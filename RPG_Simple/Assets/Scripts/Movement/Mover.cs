b5efa33e236490110fab2da91ebc80edfda29232
LAPTOP-EN852T29\zzk <thezzk@qq.com>
2020-12-14 20:11:22 +0800

    Scene management & Saving

--- RPG_Simple/Assets/Scripts/Movement/Mover.cs
+++ RPG_Simple/Assets/Scripts/Movement/Mover.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
 
         [SerializeField] Transform target;
@@ -52,5 +55,21 @@ namespace RPG.Movement
             this.GetComponent<Animator>().SetFloat("forwardSpeed", speed);
         }
 
+        public object CaptureState()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["position"] = new SerializableVector3(transform.position);
+            data["rotation"] = new SerializableVector3(transform.eulerAngles);
+            return data;
+        }
+
+        public void RestoreState(object state)
+        {
+            Dictionary<string, object> data = (Dictionary<string, object>) state;
+            
+            GetComponent<NavMeshAgent>().Warp(((SerializableVector3)data["position"]).ToVector());
+            transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector();
+            
+        }
     }
 }
